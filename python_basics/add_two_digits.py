34aed86efe9036c43c26e45665d0921b8cf21743
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2024-02-24 17:50:44 +0100

    ADD new files

---
+++ python_basics/add_two_digits.py
@@ -0,0 +1,4 @@
+num = input("Enter a two digit number: ")
+total = int(num[0]) + int(num[1])
+
+print("The sum of", num[0], "and", num[1], "is", total )
---
+++ python_basics/calc_bmi.py
@@ -0,0 +1,4 @@
+weight = int(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+bmi = weight / (height**2)
+print("Your body mass index is", bmi, "kg/m^2")
